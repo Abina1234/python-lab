78d55b7697cc1a136ffac9a82b34bd2c017d80e5
Abina S <94121200+Abina1234@users.noreply.github.com>
2022-02-08 14:59:06 +0530

    Delete pgm co2 6.py

--- pgm co2 6.py
+++
@@ -1,9 +0,0 @@
-test_str = "python programming"
-all_freq = {}
-for i in test_str:
-	if i in all_freq:
-		all_freq[i] += 1
-	else:
-		all_freq[i] = 1
-print ("Count of all characters in python programming is :\n "
-										+ str(all_freq))
