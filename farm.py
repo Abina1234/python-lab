9c3fb73b619d9a15dd627a7b27860cd4727a02df
Abina S <94121200+Abina1234@users.noreply.github.com>
2022-02-08 15:00:39 +0530

    Delete farm.py

--- farm.py
+++
@@ -1,9 +0,0 @@
-farm={"sheep":5,"cow":2,"goat":10}
-print("Elements in dictionary:",farm)
-d={"duck":8}
-farm.update(d)
-print("Elements in dictionary after update:",farm)
-c=len(farm)
-print("No:of items in dictionary:",c)
-del(farm['cow'])
-print("Elements in dictionary:",farm)
